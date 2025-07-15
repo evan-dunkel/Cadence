eb61c4c3ec9166757d9f088804f33c82d95c8baf
Evan Dunkel <43302291+evan-dunkel@users.noreply.github.com>
2025-07-14 17:57:42 -0700

    Add attribution and link to project

--- src/App.tsx
+++ src/App.tsx
@@ -10,27 +10,42 @@ function App() {
 
   return (
     <>
-      <div className="flex flex-col justify-center items-start h-screen gap-4 pl-12">
-        <div className="flex items-end gap-0.5">
-          <span className="text-6xl font-light text-stone-800">
-            Hello world{" "}
-          </span>
-          <Caret bubbleState={bubbleState} />
+      <div className="flex flex-col justify-between items-start h-screen">
+        <div className="flex-1 flex flex-col justify-center gap-4 pl-12 py-8">
+          <div className="flex items-end gap-0.5">
+            <span className="text-6xl font-light text-stone-800">
+              Hello world{" "}
+            </span>
+            <Caret bubbleState={bubbleState} />
+          </div>
+          <div className="flex items-start gap-2">
+            <Tabs
+              value={bubbleState}
+              onValueChange={(value) =>
+                setBubbleState(value as "hidden" | "icon" | "suggesting")
+              }
+              className="w-[400px]"
+            >
+              <TabsList>
+                <TabsTrigger value="hidden">Hidden</TabsTrigger>
+                <TabsTrigger value="icon">Icon</TabsTrigger>
+                <TabsTrigger value="suggesting">Suggesting</TabsTrigger>
+              </TabsList>
+            </Tabs>
+          </div>
         </div>
-        <div className="flex items-start gap-2">
-          <Tabs
-            value={bubbleState}
-            onValueChange={(value) =>
-              setBubbleState(value as "hidden" | "icon" | "suggesting")
-            }
-            className="w-[400px]"
-          >
-            <TabsList>
-              <TabsTrigger value="hidden">Hidden</TabsTrigger>
-              <TabsTrigger value="icon">Icon</TabsTrigger>
-              <TabsTrigger value="suggesting">Suggesting</TabsTrigger>
-            </TabsList>
-          </Tabs>
+        <div className="self-start pl-6 pb-4">
+          <span className="text-sm text-stone-500">
+            Created by Evan Dunkel. Proof of concept UX for{" "}
+            <a
+              className="underline"
+              href="https://evandunkel.com/cadence"
+              target="_blank"
+            >
+              Cadence
+            </a>
+            .
+          </span>
         </div>
       </div>
     </>
